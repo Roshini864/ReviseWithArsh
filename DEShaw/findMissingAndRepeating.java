dcd7d0cc1469d5d280490b7e981e0dd29b621bdc
Roshini864 <93024812+Roshini864@users.noreply.github.com>
2024-01-01 21:48:25 +0530

    Update findMissingAndRepeating.java

--- DEShaw/findMissingAndRepeating.java
+++ DEShaw/findMissingAndRepeating.java
@@ -1,7 +1,24 @@
 int[] findTwoElement(int arr[], int n) {
-        // code here
+        // bruteforce
+        int[] ans = new int[2];
+        for(int i = 1; i <= n; i++){
+            int count = 0;
+            for(int j = 0; j < arr.length; j++){
+                if(arr[j] == i){
+                    count++;
+                }
+            }
+            if(count == 2){
+                ans[0] = i;
+            }
+            if(count == 0){
+                ans[1] = i;
+            }
+        }
+        return ans;
+
+        //beter solution
         int[] count = new int[n + 1];
-        
         for(int i: arr){
             count[i]++;
         }
@@ -16,4 +33,53 @@ int[] findTwoElement(int arr[], int n) {
             }
         }
         return new int[] {rep, mis};
+
+        
+        //optimal app1
+        long SN = (n * (n + 1)) / 2;
+        long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+        long S = 0, S2 = 0;
+        for (int i = 0; i < n; i++) {
+            S += arr[i];
+            S2 += (long)arr[i] * (long)arr[i];
+        }
+        long val1 = S - SN;
+        long val2 = S2 - S2N;
+        val2 = val2 / val1;
+        long x = (val1 + val2) / 2;
+        long y = x - val1;
+        int[] ans = {(int)x, (int)y};
+        return ans;
+
+        //opt app2
+        int xr = 0;
+        for (int i = 0; i < n; i++) {
+            xr = xr^a[i];
+            xr = xr^(i + 1);
+        }
+        int number = (xr & ~(xr - 1));
+        int zero = 0;
+        int one = 0;
+        for (int i = 0; i < n; i++) {
+            if ((a[i] & number) != 0) {
+                one = one ^ a[i];
+            }
+            else {
+                zero = zero ^ a[i];
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if ((i & number) != 0) {
+                one = one ^ i;
+            }
+            else {
+                zero = zero ^ i;
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == zero) cnt++;
+        }
+        if (cnt == 2) return new int[] {zero, one};
+        return new int[] {one, zero};
     }
